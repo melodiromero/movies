dbe99621e3df7e14b6ba1d7e3d1a4cc0326f76d1
Melodi Marisel Romero <63591538+melodiromero@users.noreply.github.com>
2023-06-20 12:23:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI  # importamos FastAPI necesario para la api
+
+app = FastAPI()  # Creacion de la api
+
+@app.get("/")
+def index():  # Funcion principal
+    return {"message" :"Hola!"}
+
+@app.get("/leer_cantidad_filmaciones_mes/{mes}")
+def cantidad_filmaciones_mes(Mes:str): 
+    # Se ingresa un mes en idioma Español. 
+    # Debe devolver la cantidad de películas que fueron estrenadas en el mes consultado en la totalidad del dataset.
+    #Ejemplo de retorno: X cantidad de películas fueron estrenadas en el mes de X
+    return {"message" :Mes}
