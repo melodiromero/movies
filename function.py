e98eb78f112aad52126e55da7ded4ad24e46371a
Melodi <melodimarisel@gmail.com>
2023-06-25 23:50:21 -0300

    nuevos cambios y correcciones

--- function.py
+++ function.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 # Se importa el dataset de trabajo
 df = pd.read_csv('./dataset/dataset_limpio.csv', delimiter=',')
-
+print(df.head(1000))
 # Declaración de estructuras de datos necesarios para los metodos.
 # Diccionario de meses en español y su nro correspondiete.
 m = {
@@ -23,7 +23,7 @@ m = {
 }
 
 # Diccionario de dias en español y su nro correspondiete.
-m = {
+d = {
     'lunes':        1,
     'martes':       2,
     'miercoles':    3,
@@ -33,41 +33,47 @@ m = {
     'domingo':      7
 }
 
+def ValidarMes(mes):
+    if mes in m:
+        return 1
+    else:
+        return 0
+    
+def ValidarDia(dia):
+    if dia in d:
+        return 1
+    else:
+        return 0
 
-
-def Leer_cantidad_filmaciones_mes(mes):
+def Leer_cantidad_filmaciones_mes(mes: str):
     """ Leer_cantidad_filmaciones_mes recibe el parámetro mes tipo cadena o string, 
         y recupera del dataframe los registros, cuya pelicula se estrenaron en dicho mes.
         La función retorna un numero: 0 en casos de error o el total (en numero) de peliculas estrenadas en el mes solicitado.
     """
-   
-    # Se valida que el parámetro recibido sea una cadena y sea una clave en el diccionario m.
-    if type(mes)==str and mes in m:
-        return df[df['release_month'] ==  m[mes.lower()]].shape[0] 
-    else: 
-        return 0
+    return df[df['release_month'] ==  m[mes.lower()]].shape[0] 
     
     
-
-def Leer_cantidad_filmaciones_dia(dia):
+def Leer_cantidad_filmaciones_dia(dia: str):
     """ Leer_cantidad_filmaciones_dia recibe el parámetro dia tipo cadena o string, 
         y recupera del dataframe los registros, cuya pelicula se estrenaron en dicho dia.
         La función retorna un numero: 0 en casos de error o el total (en numero) de peliculas estrenadas en el dia solicitado.
     """
-   
-   
+
     if type(dia)==str and dia in m:
         return df[df['release_month'] ==  m[dia]].shape[0] 
     else: 
         return 0    
 
-def Leer_score_titulo(titulo):
+def Leer_score_titulo(titulo: str):
     """ Leer_score_titulo recibe el parámetro titulo tipo cadena o string, 
         y recupera del dataframe, el título, el año de estreno y el score de la pelicula.
     """
     # Se valida que el parámetro titulo sea string y se encuentre en la columna title del el dataframe.
-    if type(titulo)==str and titulo in df['title']:
-        df1 = df[df['title'] == titulo]
+    print(titulo)
+
+    if "Sabrina" in df['title']:
+        print('pasa')
+        df1 = df[df['title'].astype(str) == "Sabrina"]
 
         resultado = {  "titulo":        df1['title'].to_string(index=False),
                         "anio" :        df1['release_year'].to_string(index=False),
@@ -76,10 +82,11 @@ def Leer_score_titulo(titulo):
 
         return resultado
     else:
+        print('noo pasa')
         return 0
 
 
-# print(Leer_score_titulo("Toy Storgggy"))
+print(Leer_score_titulo("Tom and Huck"))
 """ # pruebas...
 
 print('enero', Leer_cantidad_filmaciones_mes('enero'))
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, HTTPException  # importamos FastAPI necesario para la api
-import function as f
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+# importamos FastAPI necesario para la api
+
+import function as f # importamos el archivo .py que contiene los metodos que consultan el dataset.
 
 app = FastAPI()  # Creacion de la api
 
@@ -10,16 +13,28 @@ def index():  # Funcion principal
 @app.get("/cantidad_filmaciones_mes/{mes}")
 def cantidad_filmaciones_mes(mes:str): 
     ''' Se ingresa un mes en idioma Español y la función retorna la cantidad de peliculas que se estrenaron ese mes historicamente. '''
-    
+    if f.ValidarMes(mes) == 0:
+        return JSONResponse(
+        status_code=400,
+        content={"message": "El dato ingresado no es un mes válido."},
+        )
+
     cantidad = f.Leer_cantidad_filmaciones_mes(mes)
+
     if  cantidad > 0:
         return {'mes':{mes}, 'cantidad':cantidad} 
     else:
-        raise HTTPException(status_code=400, message="El dato ingresado no es un mes válido. ")
+        raise HTTPException(status_code=400, message="Ocurrio una excepción, mes valido pero la salida es 0. ") 
 
 @app.get('/cantidad_filmaciones_dia{dia}')
 def cantidad_filmaciones_dia(dia:str):
     ''' Se ingresa el dia en idioma Español y la función retorna la cantidad de peliculas que se estrebaron ese dia historicamente. '''
+    if f.ValidarDia(dia) == 0:
+        return JSONResponse(
+        status_code=400,
+        content={"message": "El dato ingresado no es un dia válido."},
+        )
+
     cantidad = f.Leer_cantidad_filmaciones_dia(dia)
     if  cantidad > 0:
         return {'mes':{dia}, 'cantidad':cantidad} 
@@ -32,6 +47,10 @@ def score_titulo(titulo:str):
     datos = f.Leer_score_titulo(titulo)
 
     if datos == 0:
-        raise HTTPException(status_code=404, message="No hay resultados con el título de la película ingresado. ")
+        return JSONResponse(
+        status_code=404,
+        content={"message": "No hay resultados con el título de la película ingresado. "},
+        )
+        #raise HTTPException(status_code=404, message="No hay resultados con el título de la película ingresado. ")
     else:
         return datos
