7d37caa16115aca87662e93fa08f1eb87217efd4
Melodi <melodimarisel@gmail.com>
2023-07-31 23:28:41 -0300

    Primeros 3 endpoint funcionales

--- function.py
+++ function.py
@@ -1,189 +1,59 @@
 import pandas as pd
-import datetime as dt
-import ast
-import json
-from typing import Dict
-
 # Se importa el dataset de trabajo
-df  = pd.read_csv('./dataset/dataset_reducido.csv', delimiter=',')
+df  = pd.read_csv('./dataset/movies_limpio.csv', delimiter=',')
 # Declaración de estructuras de datos necesarios para los metodos.
 # Diccionario de meses en español y su nro correspondiete.
-m = {
-    'enero':        1,
-    'febrero':      2,
-    'marzo':        3,
-    'abril':        4,
-    'mayo':         5,
-    'junio':        6,
-    'julio':        7,
-    'agosto':       8,
-    'septiembre':   9,
-    'octubre':      10,
-    'noviembre':    11,
-    'diciembre':    12
-}
 
-# Diccionario de dias en español y su nro correspondiete.
-d = {
-    'lunes':        0,
-    'martes':       1,
-    'miercoles':    2,
-    'jueves':       3,
-    'viernes':      4,
-    'sabado':       5,
-    'domingo':      6
-}
+idiomas = df['original_language'].unique().tolist()
+# De los idiomas, se elimina los ivalidos 
+idiomas.remove('104.0')
+idiomas.remove('82.0')
+idiomas.remove('68.0')
+#print(idiomas)
 
-def ValidarMes(mes):
-    if mes in m:
-        return 1
-    else:
-        return 0
-    
-def ValidarDia(dia):
-    if dia in d:
+def validadIdioma(idioma):
+    if idioma in idiomas:
         return 1
     else:
         return 0
 
-def extraer_nombre(nombre_columna):
-    # Convierte el valor de la columna en un diccionario
-    if pd.isna(nombre_columna):
-        return None
-    diccionario = ast.literal_eval(nombre_columna)
-    # Extrae el valor de "name" si está presente en el diccionario
-    if isinstance(diccionario, dict) and 'name' in diccionario:
-        return diccionario['name']
-    else:
-        return None
+def leer_cantidad_peliculas_por_idioma(idioma):
+    # Se retorna la cantida de peliculas registradas con el idioma ingresado.
+    cantidad = len(df[df['original_language'] == idioma])
     
-def Leer_cantidad_filmaciones_mes(mes: str):
-    """ Leer_cantidad_filmaciones_mes recibe el parámetro mes tipo cadena o string, 
-        y recupera del dataframe los registros, cuya pelicula se estrenaron en dicho mes.
-        La función retorna un numero: 0 en casos de error o el total (en numero) de peliculas estrenadas en el mes solicitado.
-    """
-    return df[df['release_month'] ==  m[mes.lower()]].shape[0] 
-    
-    
-def Leer_cantidad_filmaciones_dia(dia: str):
-    """ Leer_cantidad_filmaciones_dia recibe el parámetro dia tipo cadena o string, 
-        y recupera del dataframe los registros, cuya pelicula se estrenaron en dicho dia.
-        La función retorna un numero: 0 en casos de error o el total (en numero) de peliculas estrenadas en el dia solicitado.
-    """
-    return df[df['release_day_of_week'] ==  d[dia.lower()]].shape[0] 
- 
+    return cantidad
 
-def Leer_score_titulo(titulo: str):
-    """ Leer_score_titulo recibe el parámetro titulo tipo cadena o string, 
-        y recupera del dataframe, el título, el año de estreno y el score de la pelicula.
-    """
-    # Se valida que el parámetro titulo sea string y se encuentre en la columna title del el dataframe.
+def leer_peliculas_duracion(pelicula):
+    # Se retorna la duracion y e año de la pelicula.
     subdicc = dict()
-    datos = df[['title', 'release_year', 'popularity']][df['title'] == titulo]
-    score = []
+    midicc = []
+    datos = df[['runtime', 'release_year']][df['title'] == pelicula]
+    
     for i in range(len(datos)):
         subdicc = {
-                    "titulo": datos.iloc[i]['title'],
-                    "anio": datos.iloc[i]['release_year'],
-                    "popularidad": datos.iloc[i]['popularity']
+                    "pelicula"  : pelicula,
+                    "duracion"  : datos.iloc[i]['runtime'],
+                    "anio"      : datos.iloc[i]['release_year']
                 }
-        score.append(subdicc)                   
-    return score  
+        midicc.append(subdicc) 
+    # Se retorna como lista de diccionarios pes hay peliculas como Sabrina que tienen mas de un registro   
+    return midicc  
 
-def Leer_votos_titulo(titulo: str):
-    """ Se ingresa el título de una filmación esperando como respuesta el título, 
-        la cantidad de votos y el valor promedio de las votaciones. 
-        La misma variable deberá de contar con al menos 2000 valoraciones, 
-        caso contrario, debemos contar con un mensaje avisando que no cumple esta condición
-        y que por ende, no se devuelve ningun valor.
-    """
-    subdicc = dict()
-    votos = []
-    df_nocumple = df[(df.title.astype(str) == titulo) & (df.vote_count < 2000)]
-    df_votos = df[(df.title.astype(str) == titulo) & ( df.vote_count >= 2000)]
-    
-    if len(df_nocumple) != 0:
-        return 1
-    
-    if len(df_votos) == 0:
-        return 0
-    else:
-        # Se recorre en el caso que haya más de un registro.
-        for i in range(len(df_votos)):
-            subdicc     = { "titulo":          df_votos.iloc[i]['title'],
-                            "anio" :           df_votos.iloc[i]['release_year'],
-                            "voto_total":      df_votos.iloc[i]['vote_average'],
-                            "voto_promedio":   df_votos.iloc[i]['vote_count']
-                        }
-        
-            votos.append(subdicc)
-        
-        return votos
-    
-"""
-def Leer_get_actor(nombre_actor: str):
-    ''' Se ingresa el nombre de un actor que se encuentre dentro del dataset debiendo devolver
-        el éxito del mismo medido a través del retorno. Además, la cantidad de películas que en 
-        las que ha participado y el promedio de retorno. '''
-    
-    contador = 0
-    suma_retorno = 0
-    lista_cast = list() 
-    sub_lista_dic = dict() 
+def leer_franquicia(franquicia):
+    # Se ingresa la franquicia y retorna la cantidad de peliculas, ganancia total y promedio. '''
+    resultados      = df[df['franquicia'] == franquicia]
+    datos           = dict()
+    cantidad        = len(resultados)
    
-    for i in range(len(df_c)): #Recorremos el dataset credits
-       
-            lista_cast = df_c.iloc[i]['cast']
-            lines = [line.strip() for line in lista_cast.split('\n') if line.strip() != ''] 
-            xy = ast.literal_eval(str(lines[0].split('"')).strip("[]").strip("'"))
-            removed_chars = ['[', ']']
- 
-            chars = set(removed_chars)
-            res = ''.join(filter(lambda x: x not in chars, xy))
-            
-            for elem in res:      #se accede  a cada elemento de la lista (en este caso cada elemento es un dictionario)
-            
-               
-                for k,v in sub_lista_dic.items(): #se accede a cada llave(k), valor(v) de cada diccionario
-                    if nombre_actor.lower == v.lower():
-                        print("se hallo", k, v)
-                
-                        
-                      
-    return 0 
+    if cantidad > 0:
+        ganancia_total  = resultados['revenue'].sum()
+        ganancia_prom   = ganancia_total / cantidad
+        datos   = {
+                        "franquicia"        : franquicia,
+                        "cantidad"          : cantidad,
+                        "ganancia_total"    : ganancia_total,
+                        "ganacia_promedio"  : ganancia_prom
+                }
+    return datos
 
-#print(Leer_votos_titulo("Toy Story"))
-#print(Leer_score_titulo("Sabrina"))
-
-
-Leer_get_actor('Juliette Binoche')
- # pruebas...
-
-print('enero', Leer_cantidad_filmaciones_mes('enero'))
-
-print('febrero', Leer_cantidad_filmaciones_mes('febrero'))
-
-print('marzo', Leer_cantidad_filmaciones_mes('marzo'))
-
-print('abril',Leer_cantidad_filmaciones_mes('abril'))
-
-print('mayo',Leer_cantidad_filmaciones_mes('mayo'))
-
-print('junio', Leer_cantidad_filmaciones_mes('junio'))
-
-print('julio', Leer_cantidad_filmaciones_mes('julio'))
-
-print('agosto', Leer_cantidad_filmaciones_mes('agosto'))
-
-print('septiembre', Leer_cantidad_filmaciones_mes('septiembre'))
-
-print('octubre', Leer_cantidad_filmaciones_mes('octubre'))
-
-print('noviembre', Leer_cantidad_filmaciones_mes('noviembre'))
-
-print('diciembre', Leer_cantidad_filmaciones_mes('diciembre'))
-
-print('diciembre', Leer_cantidad_filmaciones_mes('hhhhh'))
-
-print(Leer_score_titulo("Toy Story"))
-"""
+# print(leer_franquicia('Bad Boys Collection'))
--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request, Response, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import uvicorn
-
-from typing import Dict
+import pandas as pd
 import numpy as np
 # importamos FastAPI necesario para la api
 
@@ -18,129 +17,43 @@ if __name__ == '__main__':
 def index():  # Funcion principal
     return {"message" :"Hola!"}
 
-@app.get("/cantidad_filmaciones_mes/{mes}")
-def cantidad_filmaciones_mes(mes:str): 
-    ''' Se ingresa un mes en idioma Español y la función retorna la cantidad de peliculas que se estrenaron ese mes historicamente. '''
-    if f.ValidarMes(mes) == 0:
+@app.get('/peliculas_idioma/{idioma}')
+def peliculas_idioma( idioma: str ): 
+    '''Se ingresa un idioma y retorna la cantidad de películas producidas en ese idioma.'''
+    if f.validadIdioma(idioma) == 0:
         return JSONResponse(
         status_code=400,
-        content={"message": "El dato ingresado no es un mes válido."},
+        content={"message": "El dato ingresado no es un idioma válido."},
         )
-
-    cantidad = f.Leer_cantidad_filmaciones_mes(mes)
+   
+    cantidad = f.leer_cantidad_peliculas_por_idioma(idioma)
 
     if  cantidad > 0:
-        return {'mes':mes, 'cantidad':cantidad} 
+        return {'idioma':idioma, 'cantidad':cantidad} 
     else:
-        raise HTTPException(status_code=400, message="Ocurrio una excepción, mes valido pero la salida es 0. ") 
+        raise HTTPException(status_code=400, message="Ocurrio una excepción, idioma vàlido pero la cantidad de peliculas es 0. ") 
 
-@app.get('/cantidad_filmaciones_dia/{dia}')
-def cantidad_filmaciones_dia(dia:str):
-    ''' Se ingresa el dia en idioma Español y la función retorna la cantidad de peliculas que se estrebaron ese dia historicamente. '''
-    if f.ValidarDia(dia) == 0:
-        return JSONResponse(
-        status_code=400,
-        content={"message": "El dato ingresado no es un dia válido."},
-        )
-
-    cantidad = f.Leer_cantidad_filmaciones_dia(dia)
-  
-    if  cantidad > 0:
-        return {'dia':dia, 'cantidad':cantidad} 
-    else:
-        raise HTTPException(status_code=400, message="El dato ingresado no es un mes válido. ")
-
-@app.get('/score_titulo/{titulo}')
-def score_titulo(titulo:str):
-    '''Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score. '''
-    datos = f.Leer_score_titulo(titulo)
-
+@app.get('/peliculas_duracion/{pelicula}')
+def peliculas_duracion(pelicula:str):
+    '''Se ingresa la pelicula y retorna la duración y el año. '''
+    datos = f.leer_peliculas_duracion(pelicula)
     if len(datos) == 0:
             return JSONResponse(
             status_code=404,
             content={"message": "No existe una filmación con el titulo de película ingresado. "},
             )
-            #raise HTTPException(status_code=404, message="No hay resultados con el título de la película ingresado. ")
     else:
-        # Convertir numpy.int64 a tipos de datos compatibles
-        for item in datos:
-            for key, value in item.items():
-                if isinstance(value, np.int64):
-                    item[key] = value.item()
+        return datos
 
+@app.get('/franquicia/{franquicia}')
+def franquicia(franquicia:str):
+    '''Se ingresa la franquicia y retorna la cantidad de peliculas, ganancia total y promedio. '''
+    datos = f.leer_franquicia(franquicia)
+    if bool(datos):
+        return datos
+    else:
         return JSONResponse(
-                    status_code=200,
-                    content={"score": datos},
-                    )
-    """
-        # Convertir el diccionario a JSON serializable
-        json_resultado = jsonable_encoder(datos)
-
-        # Crear una instancia de Response con el contenido JSON
-        response = Response(content=json_resultado, media_type="application/json")
-
-        # Devolver la instancia de Response
-        return response
-"""
-
-@app.get('/votos_titulo/{titulo}')
-def votos_titulo(titulo:str):
-    ''' Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos 
-        y el valor promedio de las votaciones. La misma variable deberá de contar con al menos 2000 valoraciones, 
-        caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, 
-        no se devuelve ningun valor. '''
-    datos = ''
-    datos = f.Leer_votos_titulo(titulo)
-
-    if datos == 1:
-            return JSONResponse(
-            status_code=202,
-            content={"message": "La filmación no cumple con la condición de al menos 2000 valoraciones. "},
+            status_code=404,
+            content={"message": "No se hallaron datos con la franquicia ingresada. "},
             )
-            #raise HTTPException(status_code=404, message="No hay resultados con el título de la película ingresado. ")
-    else:
-            if datos == 0:
-                return JSONResponse(
-                status_code=404,
-                content={"message": "No existe una filmación con el titulo de película ingresado. "},
-                )
-            #raise HTTPException(status_code=404, message="No hay resultados con el título de la película ingresado. ")
-            else:          
-               
-                 # Convertir numpy.int64 a tipos de datos compatibles
-                for item in datos:
-                    for key, value in item.items():
-                        if isinstance(value, np.int64):
-                            item[key] = value.item()
-
-                return JSONResponse(
-                status_code=200,
-                content={"votos": datos},
-                )
-             
-                
-"""
-
-@app.get('/get_actor/{nombre_actor}')
-def get_actor(nombre_actor:str):
-    ''' Se ingresa el nombre de un actor que se encuentre dentro del dataset debiendo devolver
-        el éxito del mismo medido a través del retorno. Además, la cantidad de películas que en 
-        las que ha participado y el promedio de retorno. '''
-    
-    return {'actor':nombre_actor, 'cantidad_filmaciones':respuesta, 'retorno_total':respuesta, 'retorno_promedio':respuesta}
-
-@app.get('/get_director/{nombre_director}')
-def get_director(nombre_director:str):
-    ''' Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver el éxito del mismo medido a través del retorno. 
-    Además, deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.'''
-    return {'director':nombre_director, 'retorno_total_director':respuesta, 
-    'peliculas':respuesta, 'anio':respuesta,, 'retorno_pelicula':respuesta, 
-    'budget_pelicula':respuesta, 'revenue_pelicula':respuesta}
-
-# ML
-@app.get('/recomendacion/{titulo}')
-def recomendacion(titulo:str):
-    '''Ingresas un nombre de pelicula y te recomienda las similares en una lista'''
-    return {'lista recomendada': respuesta}
-    
-"""
+   
